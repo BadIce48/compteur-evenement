03c48587ab4bdfcb41c13c3cb7361a9f5e481a3a
BadIce48 <116098979+BadIce48@users.noreply.github.com>
2022-10-18 12:54:40 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,33 @@
+let Nombre_de_peronnes = 0
+input.onButtonPressed(Button.A, function () {
+    Nombre_de_peronnes += -1
+    basic.showNumber(Nombre_de_peronnes)
+})
+input.onButtonPressed(Button.AB, function () {
+    Nombre_de_peronnes = 0
+    basic.showNumber(Nombre_de_peronnes)
+})
+input.onButtonPressed(Button.B, function () {
+    Nombre_de_peronnes += 1
+    basic.showNumber(Nombre_de_peronnes)
+})
+basic.forever(function () {
+    if (Nombre_de_peronnes == 10) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # . .
+                . . . . .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+    }
+})
